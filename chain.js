3c7732bcbd8298c4b3b387daeee63c36870a8fe6
apekshajain05 <62800258+apekshajain05@users.noreply.github.com>
2020-04-20 18:48:32 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,17 @@
+class chain{
+    constructor(bodyA,bodyB){
+        var options={
+            stiffness:0.04,
+            bodyA:bodyA,
+            bodyB:bodyB,
+            length:10
+        }
+        this.chain=Matter.Constraint.create(options);
+        World.add(world,this.chain);
+    }
+    display(){
+      var A=this.chain.bodyA.position;
+      var B=this.chain.bodyB.position;
+      line(A.x,A.y,B.x,B.y);
+    }
+}
